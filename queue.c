f303894a9e588e3dbe7378d93d30bc733c9ebe22
yuto0226 <wayne71112@gmail.com>
2025-03-01 17:07:00 +0800

    Implement q_remove functions
    This commit make sure remove functions work correctly and reuses
    q_remove_head functions in q_remove_tail to eliminate special cases.
    
    Change-Id: I3edebd092b22acb51eb94e44070f23520015d7b1

--- queue.c
+++ queue.c
@@ -56,13 +56,28 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *first_ele = list_first_entry(head, element_t, list);
+
+    list_del(&first_ele->list);
+
+    if (sp && first_ele->value) {
+        strncpy(sp, first_ele->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return first_ele;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    return q_remove_head(head->prev->prev, sp, bufsize);
 }
 
 /* Return number of elements in queue */
