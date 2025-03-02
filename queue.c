c5bd3100012395f1e7cc723b162a424d57e3d692
yuto0226 <wayne71112@gmail.com>
2025-03-02 21:26:31 +0800

    Implement q_reverse function
    This commit implements the q_reverse function, which reverses the
    order of elements in the queue. The function iterates through the
    list safely and moves each node to the front of the queue to
    achieve reversal.
    
    Change-Id: I601a942d4de4c90f67c4aaf1daf8e358a9a94a26

--- queue.c
+++ queue.c
@@ -158,7 +158,13 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    struct list_head *curr = NULL, *next = NULL;
+    list_for_each_safe (curr, next, head) {
+        list_move(curr, head);
+    }
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
