6e795d6c9547ecde7160fa1fe9254f2ed8a53f60
yuto0226 <wayne71112@gmail.com>
2025-03-04 15:36:13 +0800

    Add shuffle command and 'q_shuffle' definition
    Introduce a new 'shuffle' command in 'qtest.c' to randomly shuffle
    the elements of the queue. The command performs checks to ensure the
    queue is not null and has more than one element before proceeding.
    
    Change-Id: I7991c1f62c1b363e988a03ffefe50bbcbea86914

--- qtest.c
+++ qtest.c
@@ -1056,6 +1056,36 @@ static bool do_next(int argc, char *argv[])
     return q_show(0);
 }
 
+void q_shuffle(struct list_head *head) {}
+
+static bool do_shuffle(int argc, char *argv[])
+{
+    if (argc != 1) {
+        report(1, "%s takes no arguments", argv[0]);
+        return false;
+    }
+
+    int cnt = 0;
+    if (!current || !current->q)
+        report(3, "Warning: Calling shuffle on null queue");
+    else
+        cnt = q_size(current->q);
+    error_check();
+
+    if (cnt < 2)
+        report(3, "Warning: Calling shuffle on single node");
+    error_check();
+
+    set_noallocate_mode(true);
+    if (current && exception_setup(true))
+        q_shuffle(current->q);
+    exception_cancel();
+
+    set_noallocate_mode(false);
+    q_show(3);
+    return !error_check();
+}
+
 static void console_init()
 {
     ADD_COMMAND(new, "Create new queue", "");
@@ -1096,6 +1126,7 @@ static void console_init()
                 "");
     ADD_COMMAND(reverseK, "Reverse the nodes of the queue 'K' at a time",
                 "[K]");
+    ADD_COMMAND(shuffle, "Shuffle the queue", "");
     add_param("length", &string_length, "Maximum length of displayed string",
               NULL);
     add_param("malloc", &fail_probability, "Malloc failure probability percent",
